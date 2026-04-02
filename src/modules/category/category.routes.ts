986ef509b49c26154a46de9e364f79d90f02c06b
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2026-04-02 23:34:08 +0600

    feat: category routes bug fix

--- src/modules/category/category.routes.ts
+++ src/modules/category/category.routes.ts
@@ -8,7 +8,7 @@ const router = Router();
 router.get("/", CategoryController.getAllCategories);
 router.get("/:id", CategoryController.getCategoryById);
 
-// Admin only routes
+// protected - Admin only routes
 router.post("/", auth(UserRole.ADMIN), CategoryController.createCategory);
 router.put("/:id", auth(UserRole.ADMIN), CategoryController.updateCategory);
 router.delete("/:id", auth(UserRole.ADMIN), CategoryController.deleteCategory);
